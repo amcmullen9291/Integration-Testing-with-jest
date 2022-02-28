f1e43f93b97f692bd711edf4cce22868b8d7bfd7
amcmullen <amcmullen9291@gmail.com>
2022-02-28 16:39:01 -0500

    validation integration tests

--- Notes.md
+++ Notes.md
@@ -2,3 +2,6 @@
 
 You will have a test runner (ie mocha or jest), an assertion library to define expectations (like Chai), and a Headless browser (mostly used for end to end testing)
 
+
+the "test" script  "jest --watch"  watches for changes in your app (without having to restart the server. You may have to press 'a' to start the active watching)
+
--- utils.test.js
+++ utils.test.js
@@ -6,3 +6,21 @@ test('should output name and age', () =>{
     expect(text).toBe('Anthony (42 years old)');
     //helper functions
 });
+
+test('should output dataless-text', () => {
+    const text = generateText('', null);
+    expect(text).toBe(' (null years old)');
+}) //checks for false positives (no name)
+
+test('should output dataless-text', () => {
+    const text = generateText(null);
+    expect(text).toBe('null (undefined years old)');
+}) //checks for false positives (undefined name)
+
+
+test('should generate a valid text output', () => {
+    const text = generateText('Anthony', 42);
+    expect(text).toBe('Anthony (42 years old)');
+})
+//same structure as unit test, But the tested function relies on more than
+//just one source. the two parts needed to make this function working are integrated.
